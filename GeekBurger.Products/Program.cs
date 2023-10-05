d8f63a017e04d2f225423b2a8af63841909d8f1b
Leonardo Silva <leonardo.silva@starsoft.com.br>
2023-10-04 22:12:19 -0300

    Atividade 4

--- GeekBurger.Products/Program.cs
+++ GeekBurger.Products/Program.cs
@@ -1,8 +1,43 @@
-using AutoMapper;
 using GeekBurger.Products.Repository;
-using Microsoft.EntityFrameworkCore;
+
+private const string TopicName = "ProductChangedTopic";
+private static IConfiguration _configuration;
+private const string SubscriptionName = "paulista_store";
 
 var builder = WebApplication.CreateBuilder(args);
+
+_configuration = new ConfigurationBuilder()
+  .SetBasePath(Directory.GetCurrentDirectory())
+  .AddJsonFile("appsettings.json")
+  .Build();
+
+if (!serviceBusNamespace.Topics.List()
+  .Any(t => t.Name
+  .Equals(TopicName, StringComparison.InvariantCultureIgnoreCase)))
+{
+  serviceBusNamespace.Topics
+      .Define(TopicName)
+      .WithSizeInMB(1024)
+      .Create();
+}
+
+var serviceBusNamespace =
+   _configuration.GetServiceBusNamespace();
+
+var topic = serviceBusNamespace.Topics.GetByName(TopicName);
+
+if (topic.Subscriptions.List()
+  .Any(subscription => subscription.Name
+  .Equals(SubscriptionName,
+         StringComparison.InvariantCultureIgnoreCase)))
+{
+  topic.Subscriptions.DeleteByName(SubscriptionName);
+}
+
+topic.Subscriptions
+    .Define(SubscriptionName)
+    .Create();
+
 
 // Add services to the container.
 
@@ -31,8 +66,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
@@ -43,7 +78,68 @@ app.Run();
 
 void SeedDatabase()
 {
-    using var scope = app.Services.CreateScope();
-    var productsDbContext = scope.ServiceProvider.GetRequiredService<ProductsDbContext>();
-    ProductsContextExtensions.Seed(productsDbContext);
+  using var scope = app.Services.CreateScope();
+  var productsDbContext = scope.ServiceProvider.GetRequiredService<ProductsDbContext>();
+  ProductsContextExtensions.Seed(productsDbContext);
+}
+
+static IServiceBusNamespace GetServiceBusNamespace(this IConfiguration configuration)
+{
+  var config = configuration.GetSection("serviceBus")
+               .Get<ServiceBusConfiguration>();
+
+  var credentials = SdkContext.AzureCredentialsFactory
+      .FromServicePrincipal(config.ClientId,
+                     config.ClientSecret,
+                     config.TenantId,
+                     AzureEnvironment.AzureGlobalCloud);
+
+  var serviceBusManager = ServiceBusManager
+      .Authenticate(credentials, config.SubscriptionId);
+  return serviceBusManager.Namespaces
+         .GetByResourceGroup(config.ResourceGroup,
+         config.NamespaceName);
+}
+
+static async void ReceiveMessages()
+{
+  var subscriptionClient = new SubscriptionClient(serviceBusConfiguration.ConnectionString, TopicName, SubscriptionName);
+
+  //by default a 1=1 rule is added when subscription is created, so we need to remove it
+  await subscriptionClient.RemoveRuleAsync("$Default");
+
+  await subscriptionClient.AddRuleAsync(new RuleDescription
+  {
+    Filter = new CorrelationFilter { Label = _storeId },
+    Name = "filter-store"
+  });
+
+  var mo = new MessageHandlerOptions(ExceptionHandler) { AutoComplete = true };
+
+  subscriptionClient.RegisterMessageHandler(MessageHandler, mo);
+
+  Console.ReadLine();
+}
+
+static Task MessageHandler(Message message,
+   CancellationToken arg2)
+{
+  Console.WriteLine($"message Label: {message.Label}");
+  Console.WriteLine($"CorrelationId: {message.CorrelationId}");
+  var prodChangesString = Encoding.UTF8.GetString(message.Body);
+
+  Console.WriteLine("Message Received");
+  Console.WriteLine(prodChangesString);
+
+  //Thread.Sleep(40000);
+
+  return Task.CompletedTask;
+}
+
+static Task ExceptionHandle(ExceptionReceivedEventArgs arg)
+{
+  Console.WriteLine($"Handler exception {arg.Exception}.");
+  var context = arg.ExceptionReceivedContext;
+  Console.WriteLine($"Endpoint: {context.Endpoint}, Path: {context.EntityPath}, Action: {context.Action}");
+  return Task.CompletedTask;
 }
